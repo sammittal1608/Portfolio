10867604afa3b1382998791eb4c605d4259a81cf
Saiyam Mittal <sammittal1608@gmail.com>
2024-11-28 04:48:36 +0530

    fix  resume section

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,14 +4,21 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Saiyam_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import { gtag } from "../../Utility/analytics";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
   // Handle window resizing
   useEffect(() => {
-    setWidth(window.innerWidth);
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   // Function to handle tracking resume download
@@ -34,7 +41,27 @@ function ResumeNew() {
             href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
-            onClick={trackResumeDownload} 
+            onClick={trackResumeDownload} // Add tracking here
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        {/* Resume Preview */}
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+            onClick={trackResumeDownload} // Add tracking here
           >
             <AiOutlineDownload />
             &nbsp;Download CV
